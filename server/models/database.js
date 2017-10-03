c5437576be94034750656a8924bb490112f30ffe
Ksenia <xushenka@Ksenias-MacBook-Pro.local>
2017-10-02 22:31:11 -0700

    capitalize environment variable key

--- server/models/database.js
+++ server/models/database.js
@@ -2,19 +2,19 @@ const pg = require('pg')
 const { Client } = require('pg')
 const config = require('../_config')
 
-console.log('\n\n\n\nEnv vars???', process.env.DbName)
+console.log('\n\n\n\nEnv vars???', process.env.DB_NAME)
 
 client = new Client({
   user: 'xushenka',
   host: 'localhost',
-  database: config.postgresDbName[process.env.DbName],
+  database: config.postgresDbName[process.env.DB_NAME],
   password: null,
   port: 5432,
 })
 
 client.connect((err) => {
-  if (err) console.log(`Trouble connecting to the ${config.postgresDbName[process.env.DbName]} database`)
-  else console.log(`Connected to the ${config.postgresDbName[process.env.DbName]} database`)
+  if (err) console.log(`Trouble connecting to the ${config.postgresDbName[process.env.DB_NAME]} database`)
+  else console.log(`Connected to the ${config.postgresDbName[process.env.DB_NAME]} database`)
 })
 
 module.exports = client
